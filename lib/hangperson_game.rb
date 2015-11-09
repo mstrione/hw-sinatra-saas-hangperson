45cb7058a5bd5e58e867dba7133d55147a3fb117
Mauro <mauro.strione@gmail.com>
2015-11-09 03:10:21 +0000

    Part 1 done

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,9 +8,63 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
+
+  
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ""
+    @wrong_guesses = ""
   end
+  
+  def guess(char)
+    raise ArgumentError, 'Argument error' if char.nil? || char.empty? || char.downcase == char.upcase 
+    if @word.include?char.downcase
+      if @guesses.include? char.downcase
+        return false
+      else
+        @guesses << char.downcase
+        return true
+      end
+      
+    else
+      
+      if @wrong_guesses.include? char.downcase
+        return false
+      else
+        @wrong_guesses << char.downcase
+        return true
+      end
+    end
+  end
+  
+  def word_with_guesses
+    result = ""
+    @word.each_char do |char| 
+      if @guesses.include?char
+        result << char
+      else
+        result << "-"
+      end
+    end
+    result
+  end
+  
+  def check_win_or_lose
+    if word_with_guesses.include? "-"
+      if @wrong_guesses.length>=7 
+        return :lose
+      else 
+        return :play
+      end
+    else
+      return :win    
+    end
+  end
+  
 
   def self.get_random_word
     require 'uri'
